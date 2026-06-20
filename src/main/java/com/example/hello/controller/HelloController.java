4163c99f3e2f3f60ee6be5bf66e5abe9924abe2e
Prateek Maurya <krprateek25@gmail.com>
2026-06-20 21:26:10 +0530

    Added v1 hello greeting endpoint and unit tests - corrected and added required dependencies

---
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+}
